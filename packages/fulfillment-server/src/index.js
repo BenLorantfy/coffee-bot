448f665695ae748f91c419569eb96cd7537ffd63
Ben <benlorantfy@gmail.com>
2018-03-17 16:20:00 -0400

    added body parser

--- packages/fulfillment-server/src/index.js
+++ packages/fulfillment-server/src/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser);
 
 app.get('/', (req, res) => res.send('Hello World GET!'));
-app.post('/', (req, res) => res.send('Hello World POST!'));
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World POST!');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));

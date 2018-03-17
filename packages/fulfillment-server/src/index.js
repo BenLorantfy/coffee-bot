213b1899c328551196d832a12e79224942130b1b
Ben <benlorantfy@gmail.com>
2018-03-17 17:26:47 -0400

    test

--- packages/fulfillment-server/src/index.js
+++ packages/fulfillment-server/src/index.js
@@ -4,9 +4,12 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World GET!'));
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.get('*', (req, res) => {
+  console.log('GET request');
+  res.send('Hello World GET!');
+});
+app.post('*', (req, res) => {
+  console.log('POST REQUEST', req.body);
   res.send('Hello World POST!');
 });
 

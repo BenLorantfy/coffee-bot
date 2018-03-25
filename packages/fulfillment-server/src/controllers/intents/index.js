3ba75c76c2fb12e4084acf0b801cb75581cd43d4
Ben <benlorantfy@gmail.com>
2018-03-25 19:34:24 -0400

    Added intents controller

---
+++ packages/fulfillment-server/src/controllers/intents/index.js
@@ -0,0 +1,77 @@
+const deviceId = 'coffee-bot';
+
+const intentTypes = {
+  SYNC: "action.devices.SYNC",
+  QUERY: "action.devices.QUERY",
+  EXECUTE: "action.devices.EXECUTE"
+}
+
+const deviceTypes = {
+  SWITCH: "action.devices.types.SWITCH"
+}
+
+const deviceTraits = {
+  OnOff: "action.devices.traits.OnOff"
+}
+
+const executionStatuses = {
+  SUCCESS: "SUCCESS",
+}
+
+class IntentsController {
+  process(intent) {
+    if (intent.intent === intentTypes.SYNC) {
+      return {
+        devices: [
+          {
+            id: deviceId,
+            type: deviceTypes.SWITCH,
+            traits: [
+              deviceTraits.OnOff
+            ],
+            willReportState: false,
+            name: {
+              "defaultNames": ["My Coffee Maker"],
+              "name": "Coffee Maker",
+              "nicknames": ["keurig", "coffee maker", "coffee pot"]
+            },
+            deviceInfo: {
+              manufacturer: "Lorantfy Inc",
+              model: "K50",
+              hwVersion: "1.0.0",
+              swVersion: "1.0.0"
+            }
+          }
+        ]
+      }
+    }
+
+    if (intent.intent === intentTypes.QUERY) {
+      return {
+        devices: {
+          [deviceId]: {
+            online: true
+          }
+        }
+      }
+    }
+
+    if (intent.intent === intentTypes.EXECUTE) {
+      return {
+        "commands": [{
+          "ids": [deviceId],
+          "status": executionStatuses.SUCCESS,
+          "states": {
+            "on": true
+          }
+        }]
+      }
+    }
+  }
+
+  processCommand(command) {
+
+  }
+}
+
+export default (new IntentsController());
--- packages/fulfillment-server/src/routes/fulfillment/index.js
+++ packages/fulfillment-server/src/routes/fulfillment/index.js
@@ -6,14 +6,23 @@ import { ValidationError } from '../../errors';
 import secrets from '../../../secrets';
 import jwt from 'jsonwebtoken';
 import { promisify } from 'es6-promisify';
-
+import IntentsController from '../../controllers/intents';
 
 const router = Router();
 
 router.post('/', (req, res, next) => {
   logger.info('Received some intents');
-  console.log('body', req.body);
-  return res.send(null);
+  if (!req.body && !req.body.inputs && !Array.isArray(req.body.inputs) && req.body.inputs[0]) {
+    return next(new ValidationError('Request wasn\'t formatted correctly'));
+  }
+
+  const intent = req.body.inputs[0];
+  const payload = IntentsController.process(intent);
+
+  return res.json({
+    requestId: req.body.requestId,
+    payload
+  });
 });
 
 

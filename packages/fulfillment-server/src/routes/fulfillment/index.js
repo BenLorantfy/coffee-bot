7f5fb694fd9c83dc542fe8e484b3c476614ff1a8
Ben <benlorantfy@gmail.com>
2019-02-20 17:27:18 -0500

    fix error

--- packages/fulfillment-server/src/routes/fulfillment/index.js
+++ packages/fulfillment-server/src/routes/fulfillment/index.js
@@ -22,6 +22,9 @@ router.post('/', (req, res, next) => {
 
   const token = req.headers.authorization.split(' ')[1];
   return jwtVerify(token, secrets.jwt_secret)
+    .catch((err) => {
+      next(new UnauthorizedError('Invalid JWT'))
+    })
     .then((decoded) => {
       if (!(decoded && decoded.scopes && decoded.scopes.includes('coffee.make'))) {
         return next(new UnauthorizedError('User doesn\'t have the coffee.make scope'));
@@ -34,9 +37,6 @@ router.post('/', (req, res, next) => {
           payload
         });
       });
-    })
-    .catch((err) => {
-      next(new UnauthorizedError('Invalid JWT'))
     });
 });
 
